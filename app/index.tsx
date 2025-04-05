6fca74c7b68da7f3a9a6f8507990b1f97058a93c
Rishcursion <2004rish@gmail.com>
2025-04-05 18:55:13 +0530

    chore: tested file-based routing

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,20 @@
 import { Text, View } from "react-native";
 import "../global.css";
+import { Link } from "expo-router";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-xl">Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Link href={"/onboarding"}>
+                go to onboarding
+            </Link>
+            <Text className="text-xl">Edit app/index.tsx to edit this screen.</Text>
+        </View>
+    );
 }
