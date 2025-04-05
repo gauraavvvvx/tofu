21aa780409343e16f3041f86aa6077c6e294e1c7
Rishcursion <2004rish@gmail.com>
2025-04-05 21:52:49 +0530

    feat: added card for onboarding

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,19 +1,20 @@
-import { View, Text, StyleSheet } from 'react-native';
-
+import { View } from 'react-native';
+import { TextInput, Card } from 'react-native-paper'
+import { useState } from 'react';
 export default function OnboardingScreen() {
-    return (
-        <View style={styles.container}>
-            <Text>Onboarding
-            </Text>
-        </View>
-    );
-}
+	const [text, setText] = useState("");
+	return (
+		// <Card className='flex flex-row w-full mt-12 items-center justify-center bg-slate-900'>
+		// 	<Card.Content>:w
+		//
+		// 		<TextInput label={"Name"} mode='outlined' value={text} />
+		// 	</Card.Content>
+		// </Card>
+		<View className='flex items-center justify-center'>
+			<View className='flex flex-row items-center rounded-xl justify-center border-2 border-gray-300 shadow-sm mt-12 bg-white/35 w-4/5 h-3/4'>
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-});
+			</View>
+		</View>
+	);
+}
 
